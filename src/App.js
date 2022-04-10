c14a855110ae2aa7eca4f4a6eb2c9c23daf7eefd
muzahid604 <604muzahid@gmail.com>
2022-04-10 10:45:49 +0600

    githab auth add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { getAuth, GithubAuthProvider, signInWithPopup } from "firebase/auth";
+import app from './firebase.init';
+import { useState } from 'react';
+
+const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({})
+  const signInWithGit = () => {
+    const gitProvider = new GithubAuthProvider();
+    signInWithPopup(auth, gitProvider)
+      .then((result) => {
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        const credential = GithubAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        // The signed-in user info.
+        const user = result.user;
+        setUser(user)
+        console.log(user)
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GithubAuthProvider.credentialFromError(error);
+        // ...
+      });
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={signInWithGit}>With GitHab</button>
+      <h1>{user.displayName}</h1>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
